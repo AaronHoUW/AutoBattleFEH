c5e3720db1e974b8021ff7c7a2e7bd8486e6e74f
Mario Madlife <aaronho1919@gmail.com>
2024-10-16 15:25:23 -0700

    Added mouse movement and uploaded character recongition

---
+++ character_recognition.py
@@ -0,0 +1,16 @@
+import pyautogui
+# import PIL
+
+
+character = "assets/Corrin_head.png"
+
+spriteX, spriteY = pyautogui.locateCenterOnScreen(character, grayscale=False, confidence=0.3)
+# print(spriteX, spriteY, "TEST")
+pyautogui.moveTo(spriteX, spriteY)
+
+
+# screenWidth, screenHeight = pyautogui.size()
+# print(screenWidth, screenHeight)
+
+# currentMouseX, currentMouseY = pyautogui.position()
+# print(currentMouseX, currentMouseY)
